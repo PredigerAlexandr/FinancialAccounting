9bca82ff1afc0204e005da98b196bcbeb5784f1e
Alexandr <predigeralexndr@gmail,com>
2024-01-20 12:30:24 +0400

    добавлена документация к методам

--- Application/Services/IdentityService/IdentityServiceWithDateAndGuids.cs
+++ Application/Services/IdentityService/IdentityServiceWithDateAndGuids.cs
@@ -2,13 +2,18 @@
 using Application.Services.IdentityService;
 
 namespace Application.Services;
-
+/// <summary>
+/// Сервис по работе с аутентификацией и генерацией соли
+/// </summary>
 public class IdentityServiceWithDateAndGuids:IIdentityService
 {
     private string? _challenge;
 
     public string Challenge => _challenge ?? GenerateChallenge();
-
+    /// <summary>
+    /// Метод генерации соли, отправляемой на клиент
+    /// </summary>
+    /// <returns>соль из 2 гуидов и даты</returns>
     public string GenerateChallenge()
     {
         _challenge = Guid.NewGuid().ToString() + DateTime.Now.ToString() + Guid.NewGuid().ToString();
--- FA.API/Controllers/IdentityController.cs
+++ FA.API/Controllers/IdentityController.cs
@@ -7,6 +7,10 @@ using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CreditAPI.Controllers;
+
+/// <summary>
+/// Сервис аутентификации, авторизации и идентификации 
+/// </summary>
 [ApiController]
 public class IdentityController:BaseController
 {
@@ -15,7 +19,10 @@ public class IdentityController:BaseController
     {
         _identityService = identityService;
     }
-
+    /// <summary>
+    /// действие, которое под видом проверки логина отправлет на клиент соль
+    /// </summary>
+    /// <returns></returns>
     [HttpGet]
     [Route("/checkLogin")]
     public ActionResult<string> CheckLogin()
@@ -23,6 +30,11 @@ public class IdentityController:BaseController
         return Ok(_identityService.GenerateChallenge());
     }
     
+    /// <summary>
+    /// действие, которое отвечает за регистрацию пользователя
+    /// </summary>
+    /// <param name="createUserCommand">объект пользователя из тела запроса</param>
+    /// <returns></returns>
     [Route("/registration")]
     [HttpPut]
     public async Task<ActionResult<UserDto>> Registration([FromBody] CreateUserCommand createUserCommand)
